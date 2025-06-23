b6e1748868bf71024dc5b215fda67d60fae9c000
kristenlee <leekristen090@gmail.com>
2025-06-22 20:23:10 -0400

    following routes

--- Following/dao.js
+++ Following/dao.js
@@ -4,14 +4,15 @@ import { v4 as uuidv4 } from "uuid";
 export async function followUser(userId, targetId) {
     const {following} = db.following;
     const newFollowing = { _id: uuidv4(), user: userId, target: targetId};
-    following.push(newFollowing);
+    db.following.push(newFollowing);
     return newFollowing;
 }
 export async function unfollowUser(userId, targetId) {
     const {following} = db.following;
-    const initialLength = following.length;
-    db.following = following.filter(f => !(f.user === userId && f.target === targetId));
-    return initialLength !== db.following.length;
+    // const initialLength = following.length;
+    // db.following = following.filter(f => !(f.user === userId && f.target === targetId));
+    // return initialLength !== db.following.length;
+    db.following = db.following.filter(f => !(f.user === userId && f.target === targetId));
 }
 export async function findFollowingForUser(userId) {
     return db.following.filter(f => f.user === userId);
